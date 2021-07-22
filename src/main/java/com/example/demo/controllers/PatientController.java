cb55c33dc0b2daf49b44c061edfb6e722385da10
makskayunPC <maks.kayn@yandex.com>
2021-07-22 16:50:02 +0300

    add custom methods for patients
    ...for start web

--- src/main/java/com/example/demo/controllers/PatientController.java
+++ src/main/java/com/example/demo/controllers/PatientController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Patient;
+import com.example.demo.repositories.DoctorRepository;
 import com.example.demo.repositories.PatientRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PatientController {
     private final PatientRepository patientRepository;
+    private final DoctorRepository doctorRepository;
 
-    public PatientController(PatientRepository patientRepository) {
+    public PatientController(PatientRepository patientRepository, DoctorRepository doctorRepository) {
         this.patientRepository = patientRepository;
+        this.doctorRepository = doctorRepository;
     }
 
     @GetMapping("/patients")
@@ -26,5 +31,15 @@ public class PatientController {
     @DeleteMapping("/patients/{id}")
     void delete(@PathVariable Integer id) {
         patientRepository.deleteById(id);
+    }
+
+    @GetMapping("patients/bydoctor/{id}")
+    List<Patient> findByDoctor(@PathVariable Integer id) {
+        return patientRepository.findByDoctor(doctorRepository.findById(id).get());
+    }
+
+    @GetMapping("patients/bydoctor/current/{id}")
+    List<Patient> findByDoctorCurrent(@PathVariable Integer id) {
+        return patientRepository.findByDoctorAndDischargeDateIsNull(doctorRepository.findById(id).get());
     }
 }
